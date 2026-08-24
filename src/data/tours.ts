4c7bb0adcd1564529a7ccf63905f0a88f092ab5c
LTasaycoQ <luistasayco3030@gmail.com>
2026-08-24 10:00:29 -0500

    Actualizacion de Img _ F-png enetrga de carga

--- src/data/tours.ts
+++ src/data/tours.ts
@@ -7,7 +7,7 @@ export const toursData: Tour[] = [
     title: "Perú Espectacular",
     category: "lujo",
     difficultyLevel: 2,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1778686961/iStock-1388676876_nf7wfz.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1778686961/iStock-1388676876_nf7wfz.jpg",
     link: "/itinerarios/peru-espectacular",
     descriptionKey: "peru_espectacular_desc",
     tagKey: "tag_lujo"
@@ -17,7 +17,7 @@ export const toursData: Tour[] = [
     title: "Perú Mágico",
     category: "aventura",
     difficultyLevel: 3,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/Titicaca-Lake-portada2_jfbusw.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/Titicaca-Lake-portada2_jfbusw.jpg",
     link: "/itinerarios/peru-magico",
     descriptionKey: "peru_magico_desc",
     tagKey: "tag_aventura"
@@ -27,7 +27,7 @@ export const toursData: Tour[] = [
     title: "Perú de Maravilla",
     category: "culturales",
     difficultyLevel: 1,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/banner-about-us3_w9sapa.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/banner-about-us3_w9sapa.jpg",
     link: "/itinerarios/peru-maravilla",
     descriptionKey: "peru_maravilla_desc",
     tagKey: "tag_cultural"
@@ -37,7 +37,7 @@ export const toursData: Tour[] = [
     title: "Perú en Jeans",
     category: "familias",
     difficultyLevel: 1,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/luxury-train_i18flh.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/luxury-train_i18flh.jpg",
     link: "/itinerarios/peru-jeans",
     descriptionKey: "peru_jeans_desc",
     tagKey: "tag_familias"
@@ -47,7 +47,7 @@ export const toursData: Tour[] = [
     title: "Perú Express",
     category: "grupos",
     difficultyLevel: 2,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/oasis-de-Huacachina-ica-paracas-nazca_flmink.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/oasis-de-Huacachina-ica-paracas-nazca_flmink.jpg",
     link: "/itinerarios/peru-express",
     descriptionKey: "peru_express_desc",
     tagKey: "tag_grupos"
@@ -57,7 +57,7 @@ export const toursData: Tour[] = [
     title: "Perú Esencial",
     category: "grupos",
     difficultyLevel: 2,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/mapi-portada_vvfhnw.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/mapi-portada_vvfhnw.jpg",
     link: "/itinerarios/peru-esencial",
     descriptionKey: "peru_esencial_desc",
     tagKey: "tag_grupos"
@@ -67,7 +67,7 @@ export const toursData: Tour[] = [
     title: "Majestad Inca",
     category: "grupos",
     difficultyLevel: 2,
-    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771514450/mapi-portada_vvfhnw.jpg",
+    image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_png,q_auto/v1771514450/mapi-portada_vvfhnw.jpg",
     link: "/itinerarios/majestad-inca",
     descriptionKey: "majestad_inca_desc",
     tagKey: "tag_grupos"
