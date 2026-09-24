252dec91fc6611316c7445540711ff15905497ba
LTasaycoQ <luistasayco3030@gmail.com>
2026-09-24 11:43:58 -0500

    actualizacion de page

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -28,7 +28,7 @@ export const navRoutes: NavRoute[] = [
 ];
 
 export const languages = [
-  { code: 'es' as Locale, label: 'Español',   flag: '🇵🇪', code_pais: 'es', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/v1790021194/peru_bandera_a2bqvb.png" },
-  { code: 'en' as Locale, label: 'English',   flag: '🇺🇸', code_pais: 'en', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/v1771217543/eeuu_zp1ijv.png" },
-  { code: 'pt' as Locale, label: 'Português', flag: '🇧🇷', code_pais: 'pt-BR', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/v1776789189/Flag_of_Brazil.svg_dcr2kp.png" },
+  { code: 'es' as Locale, label: 'Español',   flag: '🇵🇪', code_pais: 'es', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771217543/spanol_nisxya.png" },
+  { code: 'en' as Locale, label: 'English',   flag: '🇺🇸', code_pais: 'en', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1771217543/eeuu_zp1ijv.png" },
+  { code: 'pt' as Locale, label: 'Português', flag: '🇧🇷', code_pais: 'pt-BR', image: "https://res.cloudinary.com/dlgeap8h0/image/upload/f_auto,q_auto/v1776789189/Flag_of_Brazil.svg_dcr2kp.png" },
 ];
